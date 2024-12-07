52fd82fd8fd493bdd93336f3de0384c232bb00b7
BogdanJ33 <bogdanjanosevic@gmail.com>
2024-12-07 16:42:14 +0100

    Added sparql query embedings

---
+++ generate_sparql_queries_embedings.py
@@ -0,0 +1,58 @@
+import os
+import faiss
+import json
+import requests
+import numpy as np
+
+API_URL = "https://api.openai.com/v1/embeddings"
+API_KEY = os.getenv("OPENAI_API_KEY")
+embedding_file = "data/sparql_query_embeddings.json"
+with open("data/sparql_queries.json", "r", encoding="utf-8") as f:
+    queries_json = json.load(f)
+    
+    
+def load_embeddings(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return np.array(json.load(f), dtype=np.float32) 
+    
+    
+def save_embeddings(embeddings, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(embeddings.tolist(), f)
+        
+
+def get_embedding(text, model="text-embedding-ada-002"):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "input": text,
+        "model": model
+    }
+    response = requests.post(API_URL, headers=headers, json=data)
+    response_data = response.json()
+    return response_data["data"][0]["embedding"]
+
+
+def create_embeddings():
+    query_embeddings = []
+    for query in queries_json["queries"]:
+        embedding = get_embedding(query["question"])
+        query_embeddings.append(embedding)
+    return query_embeddings
+
+
+# query_embeddings = np.array(create_embeddings(), dtype=np.float32)
+# save_embeddings(query_embeddings, embedding_file)
+
+
+def find_best_query(user_question):
+    query_embeddings = load_embeddings(embedding_file)
+    dimension = len(query_embeddings[0]) 
+    faiss_index = faiss.IndexFlatL2(dimension)
+    faiss_index.add(query_embeddings) 
+    user_embedding = np.array(get_embedding(user_question), dtype=np.float32).reshape(1, -1)
+    distances, indices = faiss_index.search(user_embedding, k=1)
+    best_query_index = indices[0][0]
+    return queries_json["queries"][best_query_index]["query"], queries_json["queries"][best_query_index]["answer_value"]
---
+++ server.py
@@ -0,0 +1,15 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+from generate_sparql_queries_embedings import find_best_query
+
+virtuoso_url = "http://localhost:8890/sparql"
+sparql = SPARQLWrapper(virtuoso_url)
+while(True):
+    user_input = input("Postavite pitanje: ")
+    if(user_input == "x"):
+        break
+    best_query, answer_value = find_best_query(user_input)
+    sparql.setQuery(f"{best_query}")
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        print(f"{result[answer_value]['value']}")
